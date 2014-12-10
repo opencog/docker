f191fa856b3929e4b6f65acccbb1508dead1f7af
Linas Vepstas <linasvepstas@gmail.com>
2014-12-10 13:01:38 -0600

    get devel/setup.sh sourced!

--- indigo/animation/dmitry.sh
+++ indigo/animation/dmitry.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-source /.bash_profile
+source devel/setup.sh
 echo "Starting... this will take 15-20 seconds..."
 byobu new-session -d -n 'roscore' 'roscore; $SHELL'
 # byobu new-window -n 'blender' 'cd /catkin_ws/src/robo_blender/src && blender robo.blend --enable-autoexec --python  startup.py; $SHELL'
 sleep 2;
 byobu new-window -n 'blender' 'cd /catkin_ws/src/robo_blender/src && blender dmitry-mesh.anim_test.blend --enable-autoexec --python startup.py; $SHELL'
 sleep 6;
-byobu new-window -n 'cmdline' 'echo \"going into animation mode ...\"; rostopic pub --once /cmd_blendermode std_msgs/String Animations; clear; cat /catkin_ws/demo-hints.txt; $SHELL'
+byobu new-window -n 'cmdline' 'echo \"going into animation mode ...\"; rostopic pub --once /cmd_blendermode std_msgs/String Animations; clear; cat /catkin_ws/demo-hints.txt; $SHELL -l' 
 
 # Fix the annoying byobu display
 echo "tmux_left=\"session\"" > $HOME/.byobu/status
