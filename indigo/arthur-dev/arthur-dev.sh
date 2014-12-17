11a9132a58c683d01914fb63414fa247a6b57a64
Linas Vepstas <linasvepstas@gmail.com>
2014-12-16 23:42:28 -0600

    plug everything together

--- indigo/arthur-dev/arthur-dev.sh
+++ indigo/arthur-dev/arthur-dev.sh
@@ -10,11 +10,27 @@
 
 source devel/setup.sh
 echo "Starting... this will take 15-20 seconds..."
+
+# Use byobu so that the scroll bars actually work
 byobu new-session -d -n 'roscore' 'roscore; $SHELL'
 sleep 2;
-byobu new-window -n 'blender' 'cd /catkin_ws/src/robo_blender/src && blender animate-test.blend --enable-autoexec --python startup.py; $SHELL'
+tmux new-window -n 'arthur' 'roslaunch robots_config arthur-dev.launch; $SHELL'
 sleep 6;
-byobu new-window -n 'cmdline' 'echo \"going into animation mode ...\"; rostopic pub --once /cmd_blendermode std_msgs/String Animations; clear; cat /catkin_ws/demo-hints.txt; $SHELL -l' 
+
+# Start web server for providing a nice UI for the animations.
+tmux new-window -n 'rosbridge' 'roslaunch rosbridge_server rosbridge_websocket.launch'
+tmux new-window -n 'webserver' 'cd /catkin_ws/src/ros_motors_webui && python -m SimpleHTTPServer 80; $SHELL'
+
+# One blender session for object tracking, another for animations
+tmux new-window -n 'blndr-trk' 'cd /catkin_ws/src/robo_blender/src && blender robo.blend --enable-autoexec --python startup.py; $SHELL'
+tmux new-window -n 'blndr-anim' 'cd /catkin_ws/src/robo_blender/src && blender animate-test.blend --enable-autoexec --python startup.py; $SHELL'
+
+# Video camera and face tracker
+tmux new-window -n 'camera' 'roslaunch ros2opencv uvc_cam.launch'
+tmux new-window -n 'face' 'roslaunch pi_face_tracker face_tracker_uvc_cam.launch'
+
+# Spare-usage shell
+tmux new-window -n 'bash' '$SHELL'
 
 # Fix the annoying byobu display
 echo "tmux_left=\"session\"" > $HOME/.byobu/status
@@ -22,4 +38,3 @@ echo "tmux_right=\"load_average disk_io date time\"" >> $HOME/.byobu/status
 tmux attach
 
 echo "Started"
-$SHELL
