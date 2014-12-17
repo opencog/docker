85cf661eda5995eaf29f836dbe7c531c0d58c26d
Linas Vepstas <linasvepstas@gmail.com>
2014-12-16 22:26:05 -0600

    iRename per current code

--- indigo/animation/arthur.sh
+++ indigo/animation/arthur.sh
@@ -10,7 +10,7 @@ source devel/setup.sh
 echo "Starting... this will take 15-20 seconds..."
 byobu new-session -d -n 'roscore' 'roscore; $SHELL'
 sleep 2;
-byobu new-window -n 'blender' 'cd /catkin_ws/src/robo_blender/src && blender dmitry-mesh.anim_test.blend --enable-autoexec --python startup.py; $SHELL'
+byobu new-window -n 'blender' 'cd /catkin_ws/src/robo_blender/src && blender animate-test.blend --enable-autoexec --python startup.py; $SHELL'
 sleep 6;
 byobu new-window -n 'cmdline' 'echo \"going into animation mode ...\"; rostopic pub --once /cmd_blendermode std_msgs/String Animations; clear; cat /catkin_ws/demo-hints.txt; $SHELL -l' 
 
