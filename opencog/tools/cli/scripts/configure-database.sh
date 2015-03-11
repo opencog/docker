a58dff7af7a91cd732e361f5252b561ccfdb4134
Amen Belayneh <amenbelayneh@gmail.com>
2015-03-11 18:44:14 +0800

    documentation and cleanup

--- opencog/tools/cli/scripts/configure-database.sh
+++ opencog/tools/cli/scripts/configure-database.sh
@@ -7,7 +7,7 @@ psql -U postgres -h db -c "CREATE DATABASE cogdata OWNER opencog"
 
 
 # 'cheese' is the password for the next step
-cat $HOME/opencog/persist/sql/atom.sql | psql cogdata -U -W -h db
+cat $HOME/opencog/persist/sql/atom.sql | psql cogdata -U -h db
 
 # For accessing the database run
 # psql -h db -d cogdata
