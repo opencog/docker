dcd2ac644f7699390947851ea5fdc1457536dcbf
Amen Belayneh <amenbelayneh@gmail.com>
2015-03-11 18:15:29 +0800

    add persistance

---
+++ opencog/tools/cli/configure-database.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# This is a sample configuration for opencog
+
+psql -U postgres -h db -c "CREATE USER opencog WITH SUPERUSER PASSWORD 'cheese'"
+psql -U postgres -h db -c "CREATE DATABASE cogdata OWNER opencog"
+
+
+# 'cheese' is the password for the next step
+cat $HOME/opencog/persist/sql/atom.sql | psql cogdata -U -W -h db
+
+# For accessing the database run
+# psql -h db -d cogdata
