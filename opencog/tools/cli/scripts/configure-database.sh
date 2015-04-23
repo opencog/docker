6409947c338b43e7035cd8077da67a287c1e884c
Amen Belayneh <amenbelayneh@gmail.com>
2015-04-23 16:16:48 +0800

    specify the user to be used

--- opencog/tools/cli/scripts/configure-database.sh
+++ opencog/tools/cli/scripts/configure-database.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #
-# This is a sample configuration for opencog
+# This is a sample configuration for opencog that is to be run within
+# the opencog container(i.e. the container that runs opencog). The
+# environment varaible OPENCOG_SOURCE_DIR should be set for this to
+# work; if you are using docker-compose, it is set for you.
 
 psql -U postgres -h db -c "CREATE USER opencog WITH SUPERUSER PASSWORD 'cheese'"
 psql -U postgres -h db -c "CREATE DATABASE cogdata OWNER opencog"
 
-
 # 'cheese' is the password for the next step
-cat $HOME/opencog/persist/sql/atom.sql | psql cogdata -U -h db
+cat $OPENCOG_SOURCE_DIR/opencog/persist/sql/atom.sql | psql cogdata -U opencog -h db
 
 # For accessing the database run
 # psql -h db -d cogdata
