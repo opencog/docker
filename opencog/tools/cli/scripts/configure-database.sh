51cdabb6df181a2aab376dfbffa21f86aef48a51
Amen Belayneh <amenbelayneh@gmail.com>
2015-04-24 00:20:18 +0800

    add environment variables used by postgres
    includes updates for unit testing

--- opencog/tools/cli/scripts/configure-database.sh
+++ opencog/tools/cli/scripts/configure-database.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 #
-# This is a sample configuration for opencog that is to be run within
+# This is a configuration script for opencog that is to be run within
 # the opencog container(i.e. the container that runs opencog). The
 # environment varaible OPENCOG_SOURCE_DIR should be set for this to
-# work; if you are using docker-compose, it is set for you.
+# work; if you are using docker-compose, it is set for you. You might
+# have to wait for a few seconds until the postgres starts before
+# running the script. Once run you are set.
 
-psql -U postgres -h db -c "CREATE USER opencog WITH SUPERUSER PASSWORD 'cheese'"
-psql -U postgres -h db -c "CREATE DATABASE cogdata OWNER opencog"
+# For Standard Use
+## After setup for accessing the database run
+## psql -h db -U opencog_user -d mycogdata
+psql -U postgres -c "CREATE USER opencog_user WITH SUPERUSER PASSWORD 'cheese'"
 
-# 'cheese' is the password for the next step
-cat $OPENCOG_SOURCE_DIR/opencog/persist/sql/atom.sql | psql cogdata -U opencog -h db
+psql -U postgres -c "CREATE DATABASE mycogdata OWNER opencog_user"
 
-# For accessing the database run
-# psql -h db -d cogdata
+## 'cheese' is the password for the next step
+cat $OPENCOG_SOURCE_DIR/opencog/persist/sql/atom.sql | psql mycogdata -U opencog_user
+
+# For Unit testing
+psql -U postgres -c "CREATE USER opencog_tester WITH SUPERUSER PASSWORD 'cheese'"
+
+psql -U postgres -c "CREATE DATABASE opencog_test OWNER opencog_tester"
+
+## 'cheese' is the password for the next step
+cat $OPENCOG_SOURCE_DIR/opencog/persist/sql/atom.sql | psql opencog_test -U opencog_tester
