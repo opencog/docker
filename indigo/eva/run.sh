7f3a81d6ac08c176b83d52ca254740d80b4d3c3f
Linas Vepstas <linasvepstas@gmail.com>
2015-06-09 16:21:30 -0500

    install ros-indigo-angles

--- indigo/eva/run.sh
+++ indigo/eva/run.sh
@@ -6,7 +6,7 @@
 xhost +
 docker run --name="opencog-eva" --privileged  \
    -e DISPLAY=:0.0 \
-   -p 33433:33433 -p 80:80 -p 9090:9090 \
+   -p 33433:33433 -p 80:80 -p 9090:9090 -p 8080:8080 \
    -v /dev/video0:/dev/video0 -v /dev/snd:/dev/snd  \
    -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0 -v /dev/dri:/dev/dri \
    -v /dev/shm:/dev/shm -it opencog/eva
