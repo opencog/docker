85138d87cef7e4d33daa4493b44d95dd3b238de1
Amen Belayneh <amenbelayneh@gmail.com>
2015-09-08 13:30:48 +0800

    remove relex containers after run

--- opencog/relex/run-plain.sh
+++ opencog/relex/run-plain.sh
@@ -3,7 +3,7 @@
 # Run the docker container. Stop any previously running copies.
 #
 ./stop.sh relex-plain
-docker run --name="relex-plain" -p 3333:3333 \
+docker run --rm --name="relex-plain" -p 3333:3333 \
    -w /home/Downloads/relex-master opencog/relex /bin/sh plain-text-server.sh
 
 clear
