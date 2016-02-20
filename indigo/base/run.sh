8a446bc3d678b6f009dad177363d7afe42e77a94
Linas Vepstas <linasvepstas@gmail.com>
2016-02-19 23:20:08 -0600

    Add some convenience to the run command

--- indigo/base/run.sh
+++ indigo/base/run.sh
@@ -3,4 +3,9 @@
 # Run the docker container. Stop any previously running copies.
 #
 ./stop.sh
-docker run --name="opencog-indigo-base" -it opencog/ros-indigo-base
+
+# Just for fun, we will export the roscore port out of the container.
+# that way, if you run roscore inside the container, you can `rostopic
+# list` outside of it!
+docker run --name="opencog-indigo-base" \
+    --net=host -p 11311:11311 -it opencog/ros-indigo-base
