f686c2410bd42909ea7afa2fb5591af69762f3a5
Amen Belayneh <amenbelayneh@gmail.com>
2016-07-06 17:44:44 +0800

    add scripts for running postgres container for localhost usage

---
+++ opencog/postgres/README.md
@@ -0,0 +1,5 @@
+Assuming you are on a linux system, and have cloned this repository, if you want to setup postgres on your host using the docker image opencog/postgres,
+do the following
+
+1. To start run the script `./run.sh`
+2. To stop run `./stop.sh`
---
+++ opencog/postgres/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cp ../base/scripts/odbc-localhost.ini ~/.odbc.ini
+
+STARTED="FALSE"
+
+for i in $(docker ps -a --format={{.Names}}); do
+    if [ $i == "opencog-postgres" ]; then
+        echo "Starting container opencog-postgres"
+        docker start opencog-postgres
+        STARTED="TRUE"
+        echo "Started container opencog-postgres"
+    fi
+done
+
+if [ $STARTED == "FALSE" ]; then
+    echo "Starting container opencog-postgres"
+    docker run  -d --name opencog-postgres -p 5432:5432 opencog/postgres
+    echo "Started container opencog-postgres"
+fi
