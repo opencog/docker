f2103835a8d3d1087a2d65b1ecd389faae1e3ea1
Linas Vepstas <linasvepstas@gmail.com>
2023-01-14 17:18:13 -0600

    Revise the run script

--- opencog/lang-pairs/run.sh
+++ opencog/lang-pairs/run.sh
@@ -5,7 +5,7 @@
 # ./stop.sh
 
 # The -p flag is external_port:internal_port
-docker create --name pair-counter -p 17001:17001 -it opencog/lang-pairs
+docker create --name pair-counter -p 8080:80 -p 17002:17002 -it opencog/lang-pairs
 docker start -i pair-counter
 
 clear
