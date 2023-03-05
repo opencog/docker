a2f40e7aa84041d83582eab224242d6da9990913
Linas Vepstas <linasvepstas@gmail.com>
2023-03-05 14:27:20 -0600

    Start work on automation.

--- opencog/lang-pairs-auto/build.sh
+++ opencog/lang-pairs-auto/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 #
-# Build the docker image for opencog/lang-pairs
+# Build the docker image for opencog/lang-pairs-auto
 #
-docker build -t opencog/lang-pairs .
-# docker build --no-cache -t opencog/lang-pairs .
+docker build -t opencog/lang-pairs-auto .
+# docker build --no-cache -t opencog/lang-pairs-auto .
