44cf3a1a3b1436cc29c56aa4036be80186f738c9
Linas Vepstas <linasvepstas@gmail.com>
2023-03-06 18:57:00 -0600

    Misc minor bug fixes

--- opencog/lang-pairs/autorun.sh
+++ opencog/lang-pairs/autorun.sh
@@ -10,16 +10,21 @@
 PAIR_CONTAINER=pair-counter-auto
 TEXT_SOURCE=text
 
-if [[ ! "$(ls $TEXT_SOURCE)" ]; then
+if [[ -z "$(ls $TEXT_SOURCE)" ]]; then
 	echo "Error: You forgot to put an input corpus into the $TEXT_SOURCE directory!"
 	exit 1
 fi
 
 #
 # This will fail ugly, if the container already exists.
+echo "Creating container $PAIR_CONTAINER"
 docker create --name $PAIR_CONTAINER -it opencog/lang-pairs
 docker container cp $TEXT_SOURCE $PAIR_CONTAINER:/home/opencog
+
+echo "Starting container $PAIR_CONTAINER"
 docker start $PAIR_CONTAINER
 docker exec -d $PAIR_CONTAINER /home/opencog/count-pairs.sh
 sleep 10
+
+echo "Waiting for pair counting to finish in container $PAIR_CONTAINER"
 docker exec $PAIR_CONTAINER /home/opencog/count-pairs-done.sh
